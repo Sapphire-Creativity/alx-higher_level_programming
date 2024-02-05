aad7c688d923134676e7d6c2589e442738d2e551
Sapphire-Creativity <sapphirecreativity@gmail.com>
2024-02-05 07:19:53 -0800

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+"""" blah """
+    
+def is_same_class(obj, a_class):
+    """" blah """
+    obj.a_class = a_class
+    if is_same_class == a_class:
+        return True
+    return False
+print(is_same_class)
